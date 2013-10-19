a7cda16774000581136418778641383b5bd704d4
Igor Stepura <igor.stepura@gmail.com>
2013-10-19 20:30:44 +0900

    initial commit

---
+++ views.py
@@ -0,0 +1,22 @@
+from django.core.context_processors import csrf
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+from parser import *
+
+def index(request):
+    c = {}
+    c.update(csrf(request))
+    return render_to_response('interp/index.html', c)
+
+def go(request):
+    if request.method == 'POST':
+        s = request.POST["code"]
+        prs = Parser(Lexer())
+        try:
+            ast = prs.parse(s)
+            return HttpResponse(str(ast.eval({})))
+        except Exception as e:
+            return HttpResponse(str(e))
+
+
