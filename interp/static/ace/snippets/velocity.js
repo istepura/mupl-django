217bc810ee8c534ea834edd116098ddc68b9e192
Igor Stepura <igor.stepura@gmail.com>
2013-10-23 22:20:13 +0900

    Update ACE

--- interp/static/ace/snippets/velocity.js
+++ interp/static/ace/snippets/velocity.js
@@ -1,7 +1,1 @@
-define('ace/snippets/velocity', ['require', 'exports', 'module' ], function(require, exports, module) {
-
-
-exports.snippetText = "";
-exports.scope = "velocity";
-
-});
+define("ace/snippets/velocity",["require","exports","module"],function(e,t,n){t.snippetText="",t.scope="velocity"})
