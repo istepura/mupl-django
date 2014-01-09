4e1296e0b74ad58186b4ef88414a42bc64de312f
Igor Stepura <igor.stepura@gmail.com>
2014-01-09 17:28:45 +0900

    Use X-UA-Compatible in HTTP header instead of meta tag.

--- mupl_web/settings.py
+++ mupl_web/settings.py
@@ -91,12 +91,15 @@ TEMPLATE_LOADERS = (
 #     'django.template.loaders.eggs.Loader',
 )
 
+X_UA_COMPATIBLE="IE=edge,chrome=1"
+
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'compatibility.XUACompatibleMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
